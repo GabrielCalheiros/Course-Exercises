340ba51963a17eff99977cb7e4de3b38755fcde7
GabrielCalheiros <gabrielbcm.1998@gmail.com>
2023-10-27 14:32:53 -0300

    OBA

--- python/22_MultipleFunctionArguments.py
+++ python/22_MultipleFunctionArguments.py
@@ -7,11 +7,12 @@ def foo(first, second, third, *therest):
 foo(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 def bar(first, second, third, **options):
+    
     if options.get("action") == "sum": # If the action is "sum"
         print("The sum is: %d" %(first + second + third)) # Print the sum
 
     if options.get("number") == "first": # If the number is "first"
         return first # Return the first number
 
-result = bar(1, 2, 3, action = "sum", number = "first")
-print("Result: %d" %(result))
+result = bar(1, 2, 3, action = "sum", number = "first") # Call the function
+print("Result: %d" %(result)) # Print the result
